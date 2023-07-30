6d1be2b6095cfa2dd5a27ffdde8567a38afd1560
jangkh <nogovern@naver.com>
2023-07-30 16:23:36 +0900

    item repository 메소드 및 테스트 코드 추가

--- src/main/java/me/jangddol/jpashop/repository/ItemRepository.java
+++ src/main/java/me/jangddol/jpashop/repository/ItemRepository.java
@@ -3,6 +3,15 @@ package me.jangddol.jpashop.repository;
 import me.jangddol.jpashop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+        List<Item> findByItemNm(String itemNm);
+
+        List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+        List<Item> findByPriceLessThan(Integer price);
+
 
 }
