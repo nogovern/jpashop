77cc1dbdebc57fc866af32cb48a5fabb583cd041
jangkh <nogovern@naver.com>
2023-07-30 17:01:54 +0900

    itemDto 추가 및 thymeleaf 에 인수로 전달 예제 추가

--- src/main/java/me/jangddol/jpashop/controller/ThymeleafExController.java
+++ src/main/java/me/jangddol/jpashop/controller/ThymeleafExController.java
@@ -1,9 +1,12 @@
 package me.jangddol.jpashop.controller;
 
+import me.jangddol.jpashop.dto.ItemDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping(value="thymeleaf")
@@ -13,5 +16,17 @@ public class ThymeleafExController {
     public String thymeleafEx01(Model model) {
         model.addAttribute("data", "타임리프 예제입니다");
         return "thymeleafEx/thymeEx01";
+    }
+
+    @GetMapping(value="/ex02")
+    public String thymeleafEx02(Model model) {
+        ItemDto itemDto = new ItemDto();
+        itemDto.setItemDetail("상품 상세 설명");
+        itemDto.setItemNm("테스트 상품1");
+        itemDto.setPrice(10000);
+        itemDto.setRegTime(LocalDateTime.now());
+
+        model.addAttribute("itemDto", itemDto);
+        return "thymeleafEx/thymeEx02";
     }
 }
